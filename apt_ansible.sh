705ccdb0675f622d9dafebf3541fa3ee245b1471
truedevops <zhora3tonn@ukr.net>
2017-10-30 00:21:52 +0200

    изменено:      apt_ansible.sh

--- apt_ansible.sh
+++ apt_ansible.sh
@@ -3,3 +3,4 @@ sudo apt-get install software-properties-common python-software-properties
 sudo add-apt-repository -y ppa:rquillo/ansible
 sudo apt-get update
 sudo apt-get install ansible -y
+sudo rm /etc/apt/sources.list.d/rquillo-ubuntu-ansible-xenial.list
